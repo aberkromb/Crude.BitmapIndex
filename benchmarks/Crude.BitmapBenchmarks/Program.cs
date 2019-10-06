ec72b3cac100f37666fddb958e783bbd8833e479
ABERKROMB <dorgrey@mail.ru>
2019-10-06 18:25:46 +0300

    added benchmarks

---
+++ benchmarks/Crude.BitmapBenchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Crude.BitmapBenchmarks
+{
+    static class Program
+    {
+        static void Main(string[] args) =>
+            BenchmarkRunner.Run<BitmapIndex>();
+//            BenchmarkRunner.Run<Log2Benchmark>();
+//            BenchmarkRunner.Run<BitsCount>();
+    }
+}
